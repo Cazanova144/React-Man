84ac8087878e65e166d5eacf92dd2893f22ed779
Cazpian Levén <cazpians@gmail.com>
2023-01-04 14:22:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router'
+import Game from './pages/Game'
+import Home from './pages/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Game.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Game = () => {
+  return (
+    <div className="">
+        <h1 className="mb-5">Game</h1>
+
+        <div className="bg-gray-500 w-96 h-96 mx-auto my-auto">
+
+        </div>
+    </div>
+  )
+}
+
+export default Game
